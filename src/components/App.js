f3b828e114ecafb366168eba5e19f814e4652a95
PrantaChakraborty <cbpranta@gmail.com>
2022-02-17 23:35:57 +0600

    Results page created

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import Layout from "./Layout";
 // import Home from "../pages/Home";
 // import Signup from "../pages/Signup";
 // import Login from "../pages/Login";
-import Quiz from "../pages/Quiz";
+// import Quiz from "../pages/Quiz";
+import Results from "../pages/Results";
 function App() {
 	return (
     <Layout>
       {/* <Home /> */}
       {/* <Signup /> */}
       {/* <Login /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Results />
     </Layout>
   )
 }
---
+++ src/pages/Results.js
@@ -0,0 +1,11 @@
+import Summary from "../components/Summary";
+import Analysis from "../components/Analysis";
+
+export default function Results() {
+	return (
+		<>
+			<Summary />
+			<Analysis />
+		</>
+	);
+}
