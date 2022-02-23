2ffce0f0d64c82fdebe6b5aa4d0fca1956fbd9f7
PrantaChakraborty <cbpranta@gmail.com>
2022-02-23 22:09:44 +0600

    shows fetch videos on the component

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom";
 import classes from "../styles/Videos.module.css";
 import Video from "./Video";
+
+import useVideList from "../hooks/useVideoList";
 function Videos() {
+	const { loading, error, videos } = useVideList();
 	return (
 		<div className={classes.videos}>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			<Link to="/quiz">
-				<Video />
-			</Link>
-			
+			{/* if videos array length is greate than 0 */}
+			{videos.length > 0 &&
+				videos.map((video) => (
+					<Link to="/quiz" key={video.youtubeID}>
+						<Video
+							title={video.title}
+							id={video.youtubeID}
+							noq={video.noq}
+						/>
+					</Link>
+				))}
+			{!loading && videos.length === 0 && <div> No data found</div>}
+			{error && <div>There was an error</div>}
+			{loading && <div>Loading...</div>}
 		</div>
 	);
 }
