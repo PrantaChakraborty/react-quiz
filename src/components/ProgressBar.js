54164338b6697e1cc6c37275be104bc5735fe519
PrantaChakraborty <cbpranta@gmail.com>
2022-03-13 11:40:53 +0600

    implement progressbar tooltip functionality

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,18 +1,36 @@
 import Button from "./Button";
 import classes from "../styles/ProgressBar.module.css";
+import { useState, useRef } from "react";
 
 export default function ProgressBar({ next, prev, percentage, submit }) {
+	const [tooltip, setTooltip] = useState(false);
+	const tooltipRef = useRef();
+
+	function toggleTooltip() {
+		if (tooltip) {
+			setTooltip(false);
+			tooltipRef.current.style.display = "none";
+		} else {
+			setTooltip(true);
+			tooltipRef.current.style.left = `calc(${percentage}% - 65px)`;
+			tooltipRef.current.style.display = "block";
+		}
+	}
 	return (
 		<div className={classes.progressBar}>
 			<div className={classes.backButton} onClick={prev}>
 				<span className="material-icons-outlined"> arrow_back </span>
 			</div>
 			<div className={classes.rangeArea}>
-				<div className={classes.tooltip}>{percentage}% Complete!</div>
+				<div className={classes.tooltip} ref={tooltipRef}>
+					{percentage}% Complete!
+				</div>
 				<div className={classes.rangeBody}>
 					<div
 						className={classes.progress}
 						style={{ width: `${percentage}%` }}
+						onMouseOver={toggleTooltip}
+						onMouseOut={toggleTooltip}
 					></div>
 				</div>
 			</div>
